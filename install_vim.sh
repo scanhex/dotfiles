aafb4234e4d6ca346c20ee085149ab3e5d5b80bb
Alexander Morozov <scanhex@gmail.com>
2022-06-23 11:30:57 -0500

    Add installation script

---
+++ install_vim.sh
@@ -0,0 +1,10 @@
+sudo apt install git vim-gtk3
+if ! [ -f ~/.vimrc ]; then
+  curl https://raw.githubusercontent.com/scanhex/dotfiles/master/.vimrc -o ~/.vimrc
+else
+  echo "~/.vimrc exists, left unchanged."
+fi
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/zanglg/nova.vim ~/.vim/bundle/nova.vim
+mkdir -p ~/Code
+curl https://raw.githubusercontent.com/scanhex/dotfiles/master/JetBrainsMono-Regular.ttf /usr/local/share/fonts/
