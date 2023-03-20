83368bf17114c8426b63e2d8eca726dbe08d2d35
Alexander Morozov <scanhex@gmail.com>
2023-03-19 23:44:07 -0400

    Add auto move windows config

---
+++ ubuntu.sh
@@ -0,0 +1,3 @@
+# need to download https://extensions.gnome.org/extension/16/auto-move-windows/
+echo 'do not forget to download Auto-Move Windows: https://extensions.gnome.org/extension/16/auto-move-windows/'
+cat gnome-conf.dconf | dconf load /org/gnome/
