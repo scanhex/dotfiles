1096de7f53c40551402319c50879a61bf594c95f
Alexander Morozov <44344009+scanhex@users.noreply.github.com>
2023-03-31 22:08:28 -0400

    Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -5,6 +5,6 @@ else
   echo "~/.vimrc exists, left unchanged."
 fi
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/zanglg/nova.vim ~/.vim/bundle/nova.vim
+git clone https://github.com/scanhex/nova.vim ~/.vim/bundle/nova.vim
 mkdir -p ~/Code
 curl https://raw.githubusercontent.com/scanhex/dotfiles/master/JetBrainsMono-Regular.ttf /usr/local/share/fonts/
