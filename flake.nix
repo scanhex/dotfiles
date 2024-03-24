f838c32ae98198b50f907767f49831b04f766203
Alex Morozov <scanhex@gmail.com>
2024-03-24 01:36:05 +0000

    Add flake.nix, configuration.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+	description = "My master nix flake";
+
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-23.11"
+		};
+	};
+
+	outputs = { self, nixpkgs, ... }:
+		let 
+			lib = nixpkgs.lib;
+		in {
+		nixosConfigurations = {
+			nixos = lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [ ./configuration.nix ];
+			};
+		};
+	};
+}
