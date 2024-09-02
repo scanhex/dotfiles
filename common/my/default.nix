79a417e655fb070892faefcc045226e5c9aef82c
Alex Morozov <scanhex@gmail.com>
2024-09-02 01:40:44 +0000

    Add config for user

---
+++ common/my/default.nix
@@ -0,0 +1,28 @@
+{
+  inputs,
+  lib,
+  ...
+}:
+
+let
+  inherit (lib) mkOption types;
+in
+
+{
+  options.my = {
+    user = mkOption { type = types.str; };
+    name = mkOption { type = types.str; };
+    uid = mkOption { type = types.int; };
+    keys = mkOption { type = types.listOf types.singleLineStr; };
+  };
+
+  config = {
+    my = {
+      inherit (inputs.my) builder domain;
+      user = "alex";
+      name = "Alex Morozov";
+      uid = 1000;
+      keys = [ ];
+    };
+  };
+}
