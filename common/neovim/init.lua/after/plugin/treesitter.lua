e0658cc737006701786ee94da8d0c0f3066d8a95
Alex Morozov <scanhex@gmail.com>
2025-01-20 12:53:33 -0500

    Fix nvim plugins

--- common/neovim/init.lua/after/plugin/treesitter.lua
+++ common/neovim/init.lua/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter-context",
+        commit = "f6c99b64111ab1424c8fde3d9a6f3cd08234f8cb",
         opts = {
             max_lines = 20,
             min_window_height = 15,
