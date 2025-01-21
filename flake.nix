ce29d9488440bb81b0041175c636566953e2819b
Alex Morozov <scanhex@gmail.com>
2025-01-20 19:43:10 -0500

    Update to nixpkgs-24.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     inputs = {
         nixpkgs = {
-            url = "github:NixOS/nixpkgs/nixos-24.05";
+            url = "github:NixOS/nixpkgs/nixos-24.11";
         };
         nixpkgs-unstable = {
             url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -15,7 +15,7 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
         home-manager = {
-            url = "github:nix-community/home-manager/release-24.05";
+            url = "github:nix-community/home-manager/release-24.11";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
