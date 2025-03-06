4b5e7e04e4ebe4e9f99d65df111e22b3ddb925c2
Alex Morozov <scanhex@gmail.com>
2025-03-06 11:18:57 -0500

    Fix HiDPI for Steam/Discord

--- common/hyprland/default.nix
+++ common/hyprland/default.nix
@@ -5,8 +5,17 @@
   config = lib.mkIf config.my.hyprland.enable {
     programs.hyprland.enable = true;
     hm.programs.tofi.enable = true;
-    hm.home.packages = [ pkgs.blueman ];
+    hm.home.packages = [ pkgs.blueman pkgs.xorg.xrdb ];
     my.waybar.enable = true;
     my.stylix.enable = true;
+    hm.xresources.properties = {
+      "Xft.dpi" = 155;
+      "Xft.autohint" = 0;
+      "Xft.lcdfilter" = "lcddefault";
+      "Xft.hintstyle" = "hintfull";
+      "Xft.hinting" = 1;
+      "Xft.antialias" = 1;
+      "Xft.rgba" = "rgb";
+    };
   };
 }
