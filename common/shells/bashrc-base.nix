38a202d2ecf42b5bfa7b09c5baf2bc77df98496d
Alex Morozov <scanhex@gmail.com>
2025-06-27 20:01:46 +0000

    zed, bash: tab always uses AI completion; fix history size being overriden

--- common/shells/bashrc-base.nix
+++ common/shells/bashrc-base.nix
@@ -1,6 +1,6 @@
 {config, ...}:
 {
-    bashrcBase = '' 
+    bashrcBase = ''
 if [ -f /etc/bashrc ]; then
     . /etc/bashrc
 fi
@@ -12,8 +12,6 @@ if [ -f ${config.home.profileDirectory}/etc/profile.d/nix.sh ]; then
 fi
 source ${./git-prompt.sh}
 PS1='[\w$(__git_ps1 " (%s)")]\$ '
-HISTSIZE=10000000
-HISTFILESIZE=10000000
 PROMPT_COMMAND='history -a'
 export CMAKE_EXPORT_COMPILE_COMMANDS=1
 
--- common/shells/home.nix
+++ common/shells/home.nix
@@ -14,9 +14,11 @@
     };
     config = {
         programs.bash = {
-            enable = true; # otherwise bashrcExtra/shellAliases wouldn't work 
+            enable = true; # otherwise bashrcExtra/shellAliases wouldn't work
             bashrcExtra = config.my.bash.bashrcPrefix + (import ./bashrc-base.nix {config = config;}).bashrcBase + config.my.bash.bashrcSuffix;
             enableCompletion = true;
+            historyFileSize = 10000000;
+            historySize = 10000000;
         };
         #programs.zsh.enable = true;
         home.shellAliases = {
