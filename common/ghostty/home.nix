ae0e3d2d7076b5440ac1ab7855a028d48c02b435
Alex Morozov <scanhex@gmail.com>
2025-08-13 17:05:12 -0500

    Add ghostty config

--- common/ghostty/home.nix
+++ common/ghostty/home.nix
@@ -7,15 +7,17 @@
 {
     options.my.ghostty = {
         enable = lib.mkEnableOption "ghostty";
+        unmanaged = lib.mkEnableOption "ghostty not managed by nix";
     };
 
     config = lib.mkIf config.my.ghostty.enable {
         home.packages = with pkgs; [
             nerd-fonts.iosevka
         ];
-        programs.ghostty = {
+        programs.ghostty = lib.mkIf (!config.my.ghostty.unmanaged) {
             enable = true;
             package = pkgs.unstable.ghostty;
         };
+        home.file.".config/ghostty/config".source = ./config;
     };
 }
--- darwin/home.nix
+++ darwin/home.nix
@@ -8,6 +8,8 @@
 {
 
   home.packages = [ pkgs.unstable.raycast ];
+  my.ghostty.enable = true;
+  my.ghostty.unmanaged = true;
 
   imports = lib.my.getHmModules [ ./. ];
 }
